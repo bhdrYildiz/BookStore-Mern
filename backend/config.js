b2901464e58806a2b6f41de193cc0dd17339e2c9
bhdrYildiz <bahadiryildiz137@gmail.com>
2024-01-02 23:18:35 +0300

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://bahadiryildiz137:IgqSoWyI6MNQLsQS@book-store-mern.32wmj1f.mongodb.net/books-collection?retryWrites=true&w=majority";
